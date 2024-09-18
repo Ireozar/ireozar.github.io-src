b8dcf653d837839ca4f0636d09f72603b51b5efd
www-data <i.bartkowski@posteo.de>
2024-09-18 10:40:05 +0200

    upfated scripts for www-data

---
+++ webBuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+if [ ! -d ../wiki_pages/.git ]; then
+  exit 1
+fi
+../../mdbook build
+mv ../wiki_pages/.git git_files/.git
+rm -rf ../wiki_pages/*
+mv book/* ../wiki_pages/.
+mv git_files/.git ../wiki_pages/.git
+cd ../wiki_pages
+git add .
+commit_msg="$(date +"%d.%m.%y %R") (script commit)"
+git commit -m "$commit_msg"
+git push -u origin main
