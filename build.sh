8006e21d15b53a793be36ad373e23d35f62ef817
www-data <i.bartkowski@posteo.de>
2024-10-08 15:42:49 +0200

    08.10.24 15:42 (script commit)

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-if [ ! -d ../wiki_pages/.git ]; then
+if [ ! -d ../../../wiki/.git ]; then
   read -p "Confirm downloading latest version: " -n 1 -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    git clone https://github.com/ireozar/ireozar.github.io.git ../wiki_pages
+    git clone https://github.com/ireozar/ireozar.github.io.git ../../../wiki
   else
     exit 1
   fi
 fi
 ../../mdbook build
-mv ../wiki_pages/.git git_files/.git
-rm -rf ../wiki_pages/*
-mv book/* ../wiki_pages/.
-mv git_files/.git ../wiki_pages/.git
-cd ../wiki_pages
+mv ../../../wiki/.git git_files/.git
+rm -rf ../../../wiki/*
+mv book/* ../../../wiki/.
+mv git_files/.git ../../../wiki/.git
+cd ../../../wiki
 read -p "Upload? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
--- webBuild.sh
+++ webBuild.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-if [ ! -d ../wiki_pages/.git ]; then
+if [ ! -d ../../../wiki/.git ]; then
   exit 1
 fi
 ../../mdbook build
-mv ../wiki_pages/.git git_files/.git
-rm -rf ../wiki_pages/*
-mv book/* ../wiki_pages/.
-mv git_files/.git ../wiki_pages/.git
-cd ../wiki_pages
+mv ../../../wiki/.git git_files/.git
+rm -rf ../../../wiki/*
+mv book/* ../../../wiki/.
+mv git_files/.git ../../../wiki/.git
+cd ../../../wiki
 git add .
 commit_msg="$(date +"%d.%m.%y %R") (script commit)"
 git commit -m "$commit_msg"
